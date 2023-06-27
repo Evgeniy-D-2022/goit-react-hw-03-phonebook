d2aadf3af80342a93c07c886861a6fcf156f91ec
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-06-27 14:56:19 +0200

    fixed goit-react-hw-03-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,12 @@ import Notiflix from "notiflix";
 
 class App extends Component {
   state = {
-    contacts:[],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     filter: ''
   }
 
@@ -41,24 +46,6 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
-
-  componentDidUpdate (prevProps, prevState) {
-    
-    if (this.state.contacts !== prevState.contacts) {
-      console.log('Update Contacts');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-    }
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts })
-      
-    }
-  }
 
   render() {
     const { contacts } = this.state;
